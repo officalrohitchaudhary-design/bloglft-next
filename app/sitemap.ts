c96569d9e586dc812f5979bba2f3a5cb70abded5
andy-engineer009 <singhmak305@gmail.com>
2025-11-27 22:38:31 +0530

    sitemap chnages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://bloglft.com';
+  const baseUrl = 'https://blogvoom.in';
 
   return [
     {
